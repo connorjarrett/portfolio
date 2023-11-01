87cccd22557d3436bd9ea27cda8a47dbccebb726
Connor Jarrett <connor@connorjarrett.com>
2023-11-01 21:24:39 +0000

    Added customisable banners to pages

---
+++ js/banners.js
@@ -0,0 +1,25 @@
+const banner = document.querySelector(".banner")
+const container = banner.querySelector(".container")
+
+const offsetMultiplier = 1.5
+
+const distMin = (container.scrollHeight / 2 )* -1.5
+
+function resize() {
+    banner.style.marginBottom = `${container.scrollHeight / 2}px`
+}
+
+function scroll() {
+    let dist = (document.documentElement.scrollTop - container.scrollHeight / 2) * offsetMultiplier
+    let distOffset = (container.scrollHeight / 2 + dist / offsetMultiplier) * offsetMultiplier
+
+    let opacity = dist / distMin * 0.65
+
+    container.style.boxShadow = `0 25px 26px rgba(61, 61, 61, ${opacity >= 0 ? opacity : 0})`
+    container.style.translate = `0 calc(50% - ${distOffset}px)`
+}
+
+document.body.onresize = resize
+document.body.onscroll = scroll
+resize()
+scroll()
